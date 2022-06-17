ba53ff610567012828101c8f83f17d3444dad56a
Rafin_31 <asifhossain976@gmail.com>
2022-06-17 20:17:21 +0600

    about and blog updated

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -7,12 +7,7 @@ const Blog = () => {
 
                 <div className="row">
                     <div className="col-12">
-                        <h1> Difference between authorization and authentication</h1>
-                        <p>In authentication process, the identity of users are checked for providing the access to the system. While in authorization process, person's or user's authorities are checked for accessing the resources. </p>
-                        <h1> Why are you using firebase? What other options do you have to implement authentication?</h1>
-                        <p>We are using fire base for authentication and authorization. We can also use firebase  live database to store data. Other option implement authentication are Auth0, Amazon Cognito etc</p>
-                        <h1>What other services does firebase provide other than authentication</h1>
-                        <p>Firebase provide live database, cloude storage, Hosting, Cloud, Storage Google ,Analytics,Predictions,Cloud Messaging </p>
+                        <h1 className='text-center'>Coming Soon...</h1>
 
                     </div>
                 </div>
